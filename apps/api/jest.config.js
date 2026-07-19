f4aa3f3267b4d48a99a6c0b23008bea82b56c894
mahdimonir <mahdimoniruzzaman@gmail.com>
2026-07-19 12:45:55 +0600

    feat(tests): enhance coverage thresholds in Jest configuration

--- apps/api/jest.config.js
+++ apps/api/jest.config.js
@@ -4,4 +4,13 @@ module.exports = {
   roots: ["<rootDir>/src"],
   testMatch: ["**/*.test.ts", "**/*.spec.ts"],
   testPathIgnorePatterns: ["/node_modules/", "/dist/"],
+  collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
 };
